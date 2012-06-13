cc1ba556c879c1fed6e80279a8b20fda4f9678bc
Gregor Schmidt <ruby@schmidtwisser.de>
2012-06-13 22:41:14 +0200

    initial ci

---
+++ init.rb
@@ -0,0 +1,18 @@
+require 'redmine'
+require 'dispatcher'
+
+Dispatcher.to_prepare do
+  require_dependency 'bitly_host/patches/application_controller_patch'
+end
+
+Redmine::Plugin.register :bitly_host do
+  name 'Bitly Host'
+  author 'Gregor Schmidt'
+  description 'A plugin to make your ChiliProject instance a bitly host'
+  version '0.0.1'
+
+  settings(:partial => 'settings/bitly_host',
+           :default => {'username' => '',
+                        'api_key'  => ''}
+          )
+end
---
+++ lib/bitly_host/patches/application_controller_patch.rb
@@ -0,0 +1,54 @@
+require_dependency 'application_controller'
+
+module BitlyHost::Patches::ApplicationControllerPatch
+  def self.included(base)
+    base.class_eval do
+      unloadable
+
+      include InstanceMethods
+      alias_method_chain :render_404, :bitly_host
+    end
+
+    base.subclasses.each do |subclass|
+      subclass.send(:include, BitlyHost::Patches::ApplicationControllerPatch)
+    end
+  end
+
+  module InstanceMethods
+    def render_404_with_bitly_host(options = {})
+      render_404_without_bitly_host unless bitly_redirect?(self)
+    end
+
+    def render_optional_error_file(status_code)
+      case status_code
+      when 404
+        super unless bitly_redirect?(controller)
+      else
+        super
+      end
+    end
+
+    def bitly_redirect?(controller)
+      debugger
+      hash = controller.request.path[1..-1]
+
+      return false if hash.include?('/')
+
+      bitly = Bitly.new(Setting.plugin_bitly_host["username"],
+                        Setting.plugin_bitly_host["api_key"])
+
+      info = bitly.info(hash)
+
+      return false if info.long_url.blank?
+
+      redirect_to info.long_url, :status => :moved_permanently
+      true
+
+    rescue BitlyError
+      #TODO log error
+      false
+    end
+  end
+end
+
+ApplicationController.send(:include, BitlyHost::Patches::ApplicationControllerPatch)
